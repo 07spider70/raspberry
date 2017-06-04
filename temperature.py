ba9ffde3588d5a3112df1c62d28d0866cc8a1466
Jan Remen <jan.remen98@gmail.com>
2017-06-04 18:33:11 +0200

    Finalling application

--- temperature.py
+++ temperature.py
@@ -1,31 +1,46 @@
+#!/usr/bin/python
 
-
-import subprocess
+import subprocess as sub
 import re
 import RPi.GPIO as GPIO
 
-#SETUP
-pin = 18
-teplota_max = 40
+
+fan_pin = 18    #pin where is fan connected
+max_temperature = 40 #our highest comfortable temperature
+
+def setup(pin): #setup pin
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(pin, GPIO.OUT)
+
+def get_temperature():  #return value of temperature
+
+    p = subprocess.Popen('vcgencmd measure_temp', stdout = subprocess.PIPE, shell=True)
+    output, err = p.communicate()
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(pin, GPIO.OUT)
 
-#vykona prikaz cmd
-p = subprocess.Popen('vcgencmd measure_temp', stdout = subprocess.PIPE, shell=True)
-output, err = p.communicate()
-#vrati string
+    temp = re.findall(r'\d+',str(output))
+    return temp[0]
 
-#najde a ulozi teplotu do temp (cele cislo)
-temp = re.findall(r'\d+',str(output))
+def fan_on(pin):    #fan start
+    GPIO.output(pin,True)
 
-#ak je vysoka teplota pusti chladenie
-if int(temp[0]) >= teplota_max:
-	print('High ', temp[0])
-	GPIO.output(pin,True)
+def fan_off(pin):   #fan stop
+    GPIO.output(pin,False)
 
-else:
-	print('Normal') 
-	GPIO.cleanup()
+def clean_gpio(): #clean GPIO
+    GPIO.cleanup()
+
+def main(): #main function
+    setup(fan_pin)
+    try:
+        if get_temperature() >= max_temperature:
+            fan_on(fan_pin)
+        else:
+            fan_off(fan_pin)
+    except:
+        clean_gpio()
+
+if __name__ == '__main__':
+    main()
