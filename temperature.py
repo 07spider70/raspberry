e576bdd11b894ea66a3d011fdedf03294107367c
Jan Remen <jan.remen98@gmail.com>
2017-06-04 19:25:31 +0200

    Testing

--- temperature.py
+++ temperature.py
@@ -39,8 +39,11 @@ def main(): #main function
     try:
         setup(fan_pin)
         while True:
-            control()
-        sleep(5)
+            if int(get_temperature()) >= max_temperature:
+                fan_on(fan_pin)
+            else:
+                fan_off(fan_pin)
+            sleep(5)
     except:
         clean_gpio()
 
